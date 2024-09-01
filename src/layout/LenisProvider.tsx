ee39d1fbffb0e47012e72c48cc5433251fcde2cb
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2024-09-01 12:03:41 +0600

    add new page

---
+++ src/layout/LenisProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useEffect, useRef } from "react";
+
+const LenisProvider = ({ children }: { children: React.ReactNode }) => {
+  const scrollRef = useRef<HTMLElement>(null);
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll({
+        el: scrollRef.current || undefined,
+        smooth: true,
+      });
+    })();
+  }, []);
+
+  return (
+    <>
+      <main data-scroll-container ref={scrollRef}>
+        {children}
+      </main>
+    </>
+  );
+};
+
+export default LenisProvider;
+~
