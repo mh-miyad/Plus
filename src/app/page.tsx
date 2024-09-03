033a5f61b22952733c81cccd3cec74b4c1dcd565
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2024-09-03 07:33:29 +0600

    add many file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Marquee from "@/components/Marquee/Marquee";
 import StatusMarquee from "@/components/Marquee/Status";
 import LenisProvider from "@/layout/LenisProvider";
+import img_1 from "../images/img-17.jpg";
 
 export default function Home() {
   return (
     <>
       <LenisProvider>
-        <div className="h-screen bg-red-300 " data-scroll-container></div>
-        <div className="h-screen bg-green-2 flex items-center 00 ">
+        <div
+          className="h-screen hero-bar bg-blue-300 "
+          data-scroll-container
+          style={{ backgroundImage: `url(${img_1})` }}
+        ></div>
+        <div className="h-screen bg-green-2 flex items-center ">
           <Marquee />
         </div>
         <div className="h-screen bg-purple-500  " data-scroll-container>
